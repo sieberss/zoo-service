ac966906c47c36eb0f3b1a1f805726680bf7c5db
stefansiebers <s3f3s-github@yahoo.com>
2024-09-10 14:19:54 +0200

    initial commit

---
+++ src/main/java/org/example/zooservice/ZooServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.zooservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/zooservice/ZooServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zooservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
